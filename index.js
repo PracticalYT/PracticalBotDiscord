916cbbb6e9b81981dbbfd87e35bf857eceda2260
dvsazei <danielviola4457@gmail.com>
2020-10-01 18:25:38 -0400

    Remove node_modules and make some small changes

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 
-const client = new Discord.Client();
+const client = new Discord.Client()
 
 const prefix = '*'
 
-const fs = require('fs');
+const fs = require('fs')
 
-client.commands = new Discord.Collection();
+client.commands = new Discord.Collection()
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
 
-    client.commands.set(command.name, command);
+  client.commands.set(command.name, command)
 }
 
 client.once('ready', () => {
-    console.log('Dexterity is online!');
-});
+  console.log('Dexterity is online!')
+})
 
 client.on('message', message => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
+  if (!message.content.startsWith(prefix) || message.author.bot) return
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
+  const args = message.content.slice(prefix.length).split(/ +/)
+  const command = args.shift().toLowerCase()
 
-    if(command === 'ping') {
-        client.commands.get('ping').execute(message, args);
-    } else if(command == 'donate'){
-        client.commands.get('donate').execute(message, args);
-    } else if(command == 'kick'){
-        client.commands.get('kick').execute(message, args);
-    }
-});
+  if (command === 'ping') {
+    client.commands.get('ping').execute(message, args)
+  } else if (command === 'donate') {
+    client.commands.get('donate').execute(message, args)
+  } else if (command === 'kick') {
+    client.commands.get('kick').execute(message, args)
+  }
+})
 
-client.login(process.env.token);
+client.login(process.env.token)
